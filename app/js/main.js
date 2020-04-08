130665692aaeaf48595c952b5b983a182f04d23b
Shapranow <shapranow87@gmail.com>
2020-04-08 20:54:54 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,24 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        ratihg: 5,
+        starWidth: "12px",
+        normalFill: "#ffa726",
+        readOnly: true
 
-    var mixer = mixitup('.products__inner-box')
+    });
+
+    $('.products-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    var mixer = mixitup('.products__inner-box');
+
+   
     
 });
